38c09e058fb0e34f693d5b7ea92f9589ba932b5a
djazayeri <jazayeri@alum.mit.edu>
2013-05-28 17:51:59 -0700

    Cleaning up Reference Application module startup and configuration (since we have no way of forcing the referencemetadata activator to run before the registrationapp activator)

--- api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppActivator.java
+++ api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppActivator.java
@@ -14,10 +14,13 @@
 package org.openmrs.module.registrationapp;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.module.emrapi.EmrApiProperties;
 import org.openmrs.module.idgen.IdentifierSource;
 import org.openmrs.module.idgen.service.IdentifierSourceService;
@@ -28,6 +31,8 @@ import org.openmrs.module.registrationcore.RegistrationCoreConstants;
  */
 public class RegistrationAppActivator extends BaseModuleActivator {
 
+    private static final Log log = LogFactory.getLog(RegistrationAppActivator.class);
+
     @Override
     public void started() {
         setupIdentifierTypeGlobalProperties(Context.getAdministrationService(), Context.getService(IdentifierSourceService.class));
@@ -37,13 +42,19 @@ public class RegistrationAppActivator extends BaseModuleActivator {
 
     private void setupIdentifierTypeGlobalProperties(AdministrationService administrationService, IdentifierSourceService identifierSourceService) {
         // set RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID based off the autogeneration options of the primary
-        // identifier type from the EMR API module
-        EmrApiProperties emrApiProperties = Context.getRegisteredComponents(EmrApiProperties.class).iterator().next();
-        PatientIdentifierType primaryIdentifierType = emrApiProperties.getPrimaryIdentifierType();
+        // identifier type from the EMR API module, if possible. (This may not be possible if things aren't configured right,
+        // e.g. due to module startup order, in which case we log a warning and continue.)
+        try {
+            EmrApiProperties emrApiProperties = Context.getRegisteredComponents(EmrApiProperties.class).iterator().next();
+            PatientIdentifierType primaryIdentifierType = emrApiProperties.getPrimaryIdentifierType();
 
-        IdentifierSource sourceForPrimaryType = identifierSourceService.getAutoGenerationOption(primaryIdentifierType).getSource();
+            IdentifierSource sourceForPrimaryType = identifierSourceService.getAutoGenerationOption(primaryIdentifierType).getSource();
 
-        administrationService.setGlobalProperty(RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID, sourceForPrimaryType.getId().toString());
+            administrationService.setGlobalProperty(RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID, sourceForPrimaryType.getId().toString());
+        }
+        catch (Exception ex) {
+            log.warn("Failed to set global property for " + RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID + " based on " + EmrApiConstants.PRIMARY_IDENTIFIER_TYPE + ". Will try again at next module startup.", ex);
+        }
     }
 
 }
