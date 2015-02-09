44635143566e591ae93d0af1806b90c2571eca54
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-02-09 21:51:07 +0600

    RA-70 - Support for getting exact matches as a fragment action

--- omod/src/main/java/org/openmrs/module/registrationapp/fragment/controller/MatchingPatientsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/registrationapp/fragment/controller/MatchingPatientsFragmentController.java
@@ -13,11 +13,6 @@
  */
 package org.openmrs.module.registrationapp.fragment.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.openmrs.Patient;
 import org.openmrs.PersonAddress;
 import org.openmrs.PersonName;
@@ -33,6 +28,11 @@ import org.openmrs.ui.framework.annotation.SpringBean;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
@@ -44,25 +44,43 @@ public class MatchingPatientsFragmentController {
 	                                             @ModelAttribute("personName") @BindParams PersonName name,
 	                                             @ModelAttribute("personAddress") @BindParams PersonAddress address,
 	                                             HttpServletRequest request, UiUtils ui) throws Exception {
-		NavigableFormStructure formStructure = RegisterPatientFormBuilder.buildFormStructure(app);
-		
-		patient.addName(name);
-		patient.addAddress(address);
-		
-		if (formStructure != null) {
-			RegisterPatientFormBuilder.resolvePersonAttributeFields(formStructure, patient, request.getParameterMap());
-		}
-
+        addToPatient(patient, app, name, address, request);
 		List<PatientAndMatchQuality> matches = service.findFastSimilarPatients(patient, null, 2.0, 10);
-		
-		List<Patient> similarPatients = new ArrayList<Patient>();
-		for (PatientAndMatchQuality match : matches) {
-			similarPatients.add(match.getPatient());
-		}
-		
-		String[] propertiesToInclude = new String[] { "patientId", "givenName", "familyName",
-		        "patientIdentifier.identifier", "gender", "birthdate", "personAddress" };
-		
-		return SimpleObject.fromCollection(similarPatients, ui, propertiesToInclude);
+        return simplify(ui, matches);
 	}
+
+    public List<SimpleObject> getExactPatients(@RequestParam("appId") AppDescriptor app,
+                                               @SpringBean("registrationCoreService") RegistrationCoreService service,
+                                               @ModelAttribute("patient") @BindParams Patient patient,
+                                               @ModelAttribute("personName") @BindParams PersonName name,
+                                               @ModelAttribute("personAddress") @BindParams PersonAddress address,
+                                               HttpServletRequest request, UiUtils ui) throws Exception {
+        addToPatient(patient, app, name, address, request);
+        List<PatientAndMatchQuality> matches = service.findPreciseSimilarPatients(patient, null, 2.0, 10);
+        return simplify(ui, matches);
+    }
+
+    private List<SimpleObject> simplify(UiUtils ui, List<PatientAndMatchQuality> matches) {
+        List<Patient> similarPatients = new ArrayList<Patient>();
+        for (PatientAndMatchQuality match : matches) {
+            similarPatients.add(match.getPatient());
+        }
+
+        String[] propertiesToInclude = new String[] { "patientId", "givenName", "familyName",
+                "patientIdentifier.identifier", "gender", "birthdate", "personAddress" };
+
+        return SimpleObject.fromCollection(similarPatients, ui, propertiesToInclude);
+    }
+
+    private void addToPatient(Patient patient, AppDescriptor app, PersonName name, PersonAddress address, HttpServletRequest request) throws IOException {
+        NavigableFormStructure formStructure = RegisterPatientFormBuilder.buildFormStructure(app);
+
+        patient.addName(name);
+        patient.addAddress(address);
+
+        if (formStructure != null) {
+            RegisterPatientFormBuilder.resolvePersonAttributeFields(formStructure, patient, request.getParameterMap());
+        }
+    }
+
 }
