6883638c2784aa503da4dd564891ab683047779c
approce <seateq64@gmail.com>
2015-08-06 08:42:20 +0300

    Return list of identifiers to UI, not only "preferred one".

--- omod/src/main/java/org/openmrs/module/registrationapp/fragment/controller/MatchingPatientsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/registrationapp/fragment/controller/MatchingPatientsFragmentController.java
@@ -43,10 +43,10 @@ public class MatchingPatientsFragmentController {
     public static final double CUTOFF = 2.0;
 
     public static final String[] PATIENT_PROPERTIES = new String[]{"uuid", "givenName", "familyName",
-            "patientIdentifier.preferred", "patientIdentifier.identifier", "gender", "birthdate", "personAddress"};
+            "patientIdentifier.preferred", "identifier", "gender", "birthdate", "personAddress"};
 
     public static final String[] MPI_PATIENT_PROPERTIES = new String[]{"uuid", "givenName", "familyName",
-            "patientIdentifier.preferred", "patientIdentifier.identifier", "gender", "birthdate", "personAddress", "mpiPatient"};
+            "patientIdentifier.preferred", "identifier", "gender", "birthdate", "personAddress", "mpiPatient"};
 
     public List<SimpleObject> getSimilarPatients(@RequestParam("appId") AppDescriptor app,
 	                                             @SpringBean("registrationCoreService") RegistrationCoreService service,
