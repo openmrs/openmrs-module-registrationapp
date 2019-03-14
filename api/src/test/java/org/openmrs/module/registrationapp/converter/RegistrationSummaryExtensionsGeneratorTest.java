fd2072fdf140cb465e2cafa8acecd6c6ae2c8707
ruhanga <ruhanganathan@gmail.com>
2019-03-14 16:23:54 +0300

    Meaningfully renamed classes, formatted reg.summary.id, provided for when not reg.app config used and documented code.

--- api/src/test/java/org/openmrs/module/registrationapp/converter/RegistrationSummaryExtensionsGeneratorTest.java
+++ api/src/test/java/org/openmrs/module/registrationapp/converter/RegistrationSummaryExtensionsGeneratorTest.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.registrationapp.converter;
 
 import java.io.InputStream;
@@ -13,16 +26,16 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
-public class ExtensionConfigGeneratorTest {
+public class RegistrationSummaryExtensionsGeneratorTest {
 
 	private PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
 	
     @Test
-    public void generateShouldTranslateAppConfigToSummayConfig() throws Exception{
+    public void generate_shouldGenerateRegSummaryFromRegApp() throws Exception{
     	InputStream inputStream = getClass().getClassLoader().getResourceAsStream("registration_app.json");    	
     	List<AppDescriptor> appDescriptors = new ObjectMapper().readValue(inputStream, new TypeReference<List<AppDescriptor>>() {});
     	
-    	List<Extension> extensions = ExtensionConfigGenerator.generate(appDescriptors.get(0));
+    	List<Extension> extensions = RegistrationSummaryExtensionsGenerator.generate(appDescriptors.get(0));
     	
     	assertNotNull(extensions);
     	assertEquals(4, extensions.size());
