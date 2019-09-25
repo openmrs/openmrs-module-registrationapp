145b91a45b7a92350cb5be43e49a27a01bde7f4d
Amos Laboso <alaboso@pih.org>
2019-09-25 08:48:08 +0000

    New attribute, accessor, modifier for regex. Custom regex can be used to validate against value entered in this text field

--- api/src/main/java/org/openmrs/module/registrationapp/model/TextFieldWidget.java
+++ api/src/main/java/org/openmrs/module/registrationapp/model/TextFieldWidget.java
@@ -27,12 +27,23 @@ public class TextFieldWidget extends Widget {
         @JsonProperty
         private Integer size;
 
+        @JsonProperty
+        private String regex;
+
         public Integer getSize() {
             return size;
         }
 
         public void setSize(Integer size) {
             this.size = size;
+        }
+
+        public String getRegex() {
+            return regex;
+        }
+
+        public void setRegex(String regex) {
+            this.regex = regex;
         }
     }
 }
