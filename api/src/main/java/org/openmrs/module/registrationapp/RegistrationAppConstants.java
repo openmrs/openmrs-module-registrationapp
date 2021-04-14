a9947fe9ed7695e9e7bac89730863fed23cd2938
Cosmin <cioan@pih.org>
2021-04-14 14:59:27 -0400

    RA-1910, configure paper ID column in search patients table (#109)
    * RA-1910, configure paper ID column in search patients table

--- api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppConstants.java
+++ api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppConstants.java
@@ -9,4 +9,10 @@ public class RegistrationAppConstants {
      * should be distributed across the left and right columns (value "true") or not (value "false").
      */
     public static final String DISTRIBUTE_SUMMARY_WIDGETS = "registrationapp.summarywidgets.distribute";
+    
+    /**
+     * The name of the global property that controls whether to display the paper record number column on the
+     * registration search results table.
+     */
+    public static final String GP_PAPER_RECORD_IDENTIFIER_DEFINITION = "registrationapp.paperRecordIdentifierDefinition";
 }
