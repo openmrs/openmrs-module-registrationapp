d779b28513625504fc66a72520f0c0064d9e91aa
Cosmin <cioan@pih.org>
2023-10-21 01:27:42 -0400

    UHM-7475, configure person relationships widget with a specific type (#129)
    * UHM-7475, configure person relationships widget with a specific type

---
+++ omod/src/main/webapp/fragments/field/registerPersonRelationship.gsp
@@ -0,0 +1,47 @@
+<%
+    ui.includeJavascript("registrationapp", "field/registerPersonRelationship.js")
+    ui.includeCss("registrationapp","registerPatient.css")
+
+    def multipleValues = config.multipleValues ? config.multipleValues : false
+%>
+
+<div id="${ config.id }-container">
+    <div id="${ config.id }-fields-div">
+        <p>
+            <input type="hidden" id="${ config.id }-relationship_type" name="relationship_type" class="rel_type" value="" data-display-value="-"/>
+        </p>
+        <p>
+            <label for="${ config.id }-field">
+                ${ config.label } <% if (config.classes && config.classes.join(' ').contains("required")) { %><span>(${ ui.message("emr.formValidation.messages.requiredField.label") })</span><% } %>
+            </label>
+
+            <input type="text"  id="${ config.id }-field" class="searchablePerson" size="40" placeholder="${ui.message(ui.encodeHtmlAttribute('registrationapp.person.name'))}"/>
+            <input type="hidden" name="other_person_uuid" id="${ config.id }-other_person_uuid"/>
+            <% if ( multipleValues ) { %>
+                    <a style="padding-left: 20px;" class="addRelationship">
+                        <i class="icon-plus-sign edit-action"></i>
+                    </a>
+            <% } %>
+        </p>
+    </div>
+</div>
+
+<script type="text/javascript">
+  var patientRelationship = {
+    id: null,
+    relationshipType: null,
+    relationshipDirection: null,
+    container: null,
+    multipleValues: null,
+    gender: null
+  }
+
+  patientRelationship.id = '${ config.id }';
+  patientRelationship.gender = '${ config.gender ? config.gender : null }';
+  patientRelationship.multipleValues = '${ multipleValues }';
+  patientRelationship.relationshipType = '${ config.relationshipType }';
+  patientRelationship.relationshipDirection = '${ config.relationshipDirection }';
+  patientRelationship.container = jq('#${ config.id }-container');
+
+  RegisterPatientRelationship(patientRelationship);
+</script>
---
+++ omod/src/main/webapp/resources/scripts/field/registerPersonRelationship.js
@@ -0,0 +1,86 @@
+function RegisterPatientRelationship(patientRelationship) {
+
+  function getInputElementFor(fieldId) {
+    // this is to handle integration with HFE, when the id is set on the parent span,
+    // not the input element itself
+    return $('#' + fieldId).is('input') ?
+      $('#' + fieldId) :
+      $('#' + fieldId).find('input');
+  }
+
+  function getValue(fieldId) {
+    return getInputElementFor(fieldId).val();
+  }
+  function setValue(fieldId, value) {
+    getInputElementFor(fieldId).val(value);
+  }
+
+  patientRelationship.container.find(".searchablePerson").autocomplete({
+        autoFocus: true,
+        source: function (request, response) {
+          if (request.term.length < 3) {
+            return;
+          }
+          let url = '/' + OPENMRS_CONTEXT_PATH + '/ws/rest/v1/person/';
+          $.getJSON(url, {
+            limit: 10,
+            q: request.term,
+            v: 'custom:(id,uuid,display,gender,age)'
+          }, function (result) {
+              let items = null;
+              if ( patientRelationship.gender && patientRelationship.gender != 'null') {
+                items = _.filter(result.results, function(person) {
+                  return (person.gender === patientRelationship.gender);
+                });
+              }
+              let persons = _.map( items != null ? items : result.results, function (item) {
+                return {
+                  label: item.display + ", " + item.age + ", " + item.gender,
+                  data: item.uuid
+                }
+              });
+              response(persons);
+          });
+        },
+        select: function (event, ui) {
+              setValue(patientRelationship.id + "-other_person_uuid", ui.item.data);
+              if (ui.item.data) {
+                setValue(patientRelationship.id + "-relationship_type",  patientRelationship.relationshipType + "-" + patientRelationship.relationshipDirection);
+              } else {
+                setValue(patientRelationship.id + "-relationship_type", '');
+              }
+        },
+        change: function (event, ui) {
+          if ( !ui.item ) {
+            setValue(patientRelationship.id + "-relationship_type", '');
+            getInputElementFor(patientRelationship.id + "-field").focus();
+          }
+        }
+    });
+
+    patientRelationship.container.find(".addRelationship").on("click", function() {
+
+      let otherPersonName = $('#' + patientRelationship.id + '-field').val();
+      let otherPersonUuid = $('#' + patientRelationship.id + '-other_person_uuid').val();
+      if (otherPersonName && otherPersonUuid) {
+        $('<div>' +
+          '<p><input type="hidden" name="relationship_type" class="rel_type" ' +
+          'value="' + patientRelationship.relationshipType + '-' + patientRelationship.relationshipDirection
+          + '" data-display-value="-"></p>'
+          + '<p><input type="text" value="' + otherPersonName + '" data-display-value="' + otherPersonName + '" size="40" readonly>' +
+          '<input type="hidden" name="other_person_uuid" value="' + otherPersonUuid + '">'
+          + '<a class="removeRelationship" style="padding-left: 20px;"><i class="icon-minus-sign edit-action"></i></a>'
+          + '</p>'
+          + '</div>').insertBefore('#' + patientRelationship.id + '-fields-div');
+
+        $(".removeRelationship").click(function() {
+          $(this).closest('div').remove();
+        });
+
+        setValue(patientRelationship.id + "-relationship_type", '');
+        setValue(patientRelationship.id + "-field", '');
+        setValue(patientRelationship.id + "-other_person_uuid", '');
+      }
+    });
+
+}
